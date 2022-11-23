d606b106c4c34049267912251602e7622b6279cb
eljayman <lelandoj@gmail.com>
2022-11-23 16:31:43 -0700

    HTML is built with manager card

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const Manager = require("./lib/manager.js");
+const fs = require("fs");
 let newManager;
 
 const managerQuestions = [
@@ -28,7 +29,10 @@ const managerQuestions = [
 inquirer.prompt(managerQuestions).then(
   ({ empName, empID, empEmail, officeNumber }) => {
     newManager = new Manager(empName, empID, empEmail, officeNumber);
-    managerCard(newManager);
+    const contents = renderHTML();
+    fs.writeFile("./dist/index.html", contents, (error) => {
+      if (error) console.log(error);
+    });
   },
   (error) => {
     console.log(error);
@@ -36,32 +40,41 @@ inquirer.prompt(managerQuestions).then(
 );
 
 function managerCard() {
-  console.log(newManager);
-  cardBuilder();
-  nameContainer.innerText = Manager.empName;
+  return `
+  <div class="card">
+  <div id="card-header">
+    <div id="name-block"><h2>${newManager.empName}</h2></div>
+    <div id="title-block"><h3>Manager</h3></div>
+  </div>
+  <div id="card-main">
+    <div id="card-field"><h4>Employee ID:  ${newManager.empID}</h4></div>
+    <div id="card-field"><h4>Email Address:  ${newManager.empEmail}</h4></div>
+    <div id="card-field"><h4>Office Number:  ${newManager.officeNumber}</h4></div>
+  </div>
+</div>
+  `;
 }
-
-function cardBuilder() {
-  const divContainer = document.createElement("div");
-  divContainer.setAttribute("class", "card");
-  const cardHeader = document.createElement("div");
-  cardHeader.setAttribute("id", "card-header");
-  const nameContainer = document.createElement("h2");
-  nameContainer.setAttribute("id", "name-block");
-  const titleContainer = document.createElement("h3");
-  titleContainer.setAttribute("id", "title-block");
-  const cardMain = document.createElement("div");
-  cardMain.setAttribute("class", "card-main");
-  const cardField = document.createElement("div");
-  cardField.setAttribute("class", "card-field");
-
-  divContainer.append(cardHeader);
-  cardHeader.append(nameContainer);
-  cardHeader.append(titleContainer);
-  divContainer.append(cardMain);
-  cardMain.append(cardField);
-
-  console.log(divContainer.innerHTML);
+function renderHTML() {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="../dist/style.css" rel="stylesheet" />
+    <title>My Team</title>
+  </head>
+  <body>
+    <header>
+      <h1>My Team</h1>
+    </header>
+    <main>
+      ${managerCard(newManager)}
+    </main>
+  </body>
+</html>
+  `;
 }
 // const fileData = fs.readFile("./lib/manager.json", (err, data) => {
 //   if (err) {
--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,18 +1,39 @@
-const fs = require(fs);
-
-function cardBuilder() {
-  const divContainer = document.createElement("div");
-  divContainer.setAttribute("class", "card");
-  const cardHeader = document.createElement("header");
-  cardHeader.setAttribute("id", "card-header");
-  const nameContainer = document.createElement("h2");
-  nameContainer.setAttribute("id", "name-block");
-  const titleContainer = document.createElement("h3");
-  titleContainer.setAttribute("id", "title-block");
-  const cardMain = document.createElement("div");
-  cardMain.setAttribute("class", "card-main");
-  const cardField = document.createElement("div");
-  cardField.setAttribute("class", "card-field");
+function managerCard(empName, empID, empEmail, officeNumber) {
+  return `
+  <div class="card">
+  <div id="card-header">
+    <div id="name-block"><h2>${empName}</h2></div>
+    <div id="title-block"><h3>title</h3></div>
+  </div>
+  <div id="card-main">
+    <div id="card-field"><h4>${empID}</h4></div>
+    <div id="card-field"><h4>${empEmail}</h4></div>
+    <div id="card-field"><h4>${officeNumber}</h4></div>
+  </div>
+</div>
+  `;
+}
+function renderHTML(managerCard) {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="../dist/style.css" rel="stylesheet" />
+    <title>My Team</title>
+  </head>
+  <body>
+    <header>
+      <h1>My Team</h1>
+    </header>
+    <main>
+      ${managerCard}
+    </main>
+  </body>
+</html>
+  `;
 }
 
 function engineerCard() {}
